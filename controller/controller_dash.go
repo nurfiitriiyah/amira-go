05508cb990e05d3c22996eeda891bce5805ab928
nurfiiitriiiyah <nurfiitriiyah@gmail.com>
2019-06-18 15:04:53 +0700

    fix chart

--- controller/controller_dash.go
+++ controller/controller_dash.go
@@ -11,12 +11,14 @@ import (
 func (idb *InDB) GetDashboard(c *gin.Context) {
 
 	var (
-		DashboardLabel []time.Time
+		DashboardLabel           []time.Time
+		DashboardArrayCountTotal []int
 
 		DashboardArrayApproved []int
 		DashboardArrayRejected []int
 		DashboardArrayPending  []int
 
+		//DashboardArrayCount int
 		Dashboard structs.OrderDash
 		result    gin.H
 	)
@@ -28,51 +30,68 @@ func (idb *InDB) GetDashboard(c *gin.Context) {
 	}
 
 	type DashAll struct {
+		GraphCountTotal []int
 		GraphLabel      []time.Time
 		DashDetailGraph DashDetailGraph
 	}
 	orderDash, err := idb.DB.Table("tb_orders").Select("order_start_date,sum(if(order_status = 1, 1, 0)) as 'Approve',sum(if(order_status = 2, 1, 0)) as 'Rejected',sum(if(order_status = 3, 1, 0)) as 'Pending'").Group("date(order_start_date)").Order("order_start_date asc").Rows()
 	orderDashLabel, err := idb.DB.Table("tb_orders").Select("order_start_date").Group("date(order_start_date)").Order("order_start_date asc").Rows()
+	orderCountTotalData, err := idb.DB.Table("tb_orders").Select("sum(if(order_status = 1, 1, 0)) as 'Approve',sum(if(order_status = 2, 1, 0)) as 'Rejected',sum(if(order_status = 3, 1, 0)) as 'Pending'").Rows()
+
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, err)
 		c.Abort()
-	}
+	} else {
+		for orderCountTotalData.Next() {
+			err := orderCountTotalData.Scan(&Dashboard.OrderApproved, &Dashboard.OrderRejected, &Dashboard.OrderPending)
 
-	for orderDashLabel.Next() {
-		err := orderDashLabel.Scan(&Dashboard.OrderDate)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, err)
-			c.Abort()
-		} else {
-			DashboardLabel = append(DashboardLabel, Dashboard.OrderDate)
-		}
-	}
+			if err != nil {
+				fmt.Println(err)
+			}
+			DashboardArrayCountTotal = append(DashboardArrayCountTotal, Dashboard.OrderApproved)
+			DashboardArrayCountTotal = append(DashboardArrayCountTotal, Dashboard.OrderRejected)
+			DashboardArrayCountTotal = append(DashboardArrayCountTotal, Dashboard.OrderPending)
 
-	for orderDash.Next() {
-		err := orderDash.Scan(&Dashboard.OrderDate, &Dashboard.OrderApproved, &Dashboard.OrderRejected, &Dashboard.OrderPending)
-		if err != nil {
-			fmt.Println(err)
-			c.JSON(http.StatusInternalServerError, err)
-			c.Abort()
-		} else {
-			DashboardArrayApproved = append(DashboardArrayApproved, Dashboard.OrderApproved)
-			DashboardArrayRejected = append(DashboardArrayRejected, Dashboard.OrderRejected)
-			DashboardArrayPending = append(DashboardArrayPending, Dashboard.OrderPending)
 		}
 
-	}
+		for orderDashLabel.Next() {
+			err := orderDashLabel.Scan(&Dashboard.OrderDate)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, err)
+				c.Abort()
+			} else {
+				DashboardLabel = append(DashboardLabel, Dashboard.OrderDate)
+			}
+		}
 
-	result = gin.H{
-		"status": "ok",
-		"result": DashAll{
-			GraphLabel: DashboardLabel,
-			DashDetailGraph: DashDetailGraph{
-				Approve: DashboardArrayApproved,
-				Pending: DashboardArrayPending,
-				Reject:  DashboardArrayRejected,
+		for orderDash.Next() {
+			err := orderDash.Scan(&Dashboard.OrderDate, &Dashboard.OrderApproved, &Dashboard.OrderRejected, &Dashboard.OrderPending)
+			if err != nil {
+				fmt.Println(err)
+				c.JSON(http.StatusInternalServerError, err)
+				c.Abort()
+			} else {
+				DashboardArrayApproved = append(DashboardArrayApproved, Dashboard.OrderApproved)
+				DashboardArrayRejected = append(DashboardArrayRejected, Dashboard.OrderRejected)
+				DashboardArrayPending = append(DashboardArrayPending, Dashboard.OrderPending)
+			}
+
+		}
+
+		result = gin.H{
+			"status": "ok",
+			"result": DashAll{
+				GraphCountTotal: DashboardArrayCountTotal,
+				GraphLabel:      DashboardLabel,
+				DashDetailGraph: DashDetailGraph{
+					Approve: DashboardArrayApproved,
+					Pending: DashboardArrayPending,
+					Reject:  DashboardArrayRejected,
+				},
 			},
-		},
+		}
+
+		c.JSON(http.StatusOK, result)
 	}
 
-	c.JSON(http.StatusOK, result)
 }
